04f79c92b6ef522902699f0b429dd42cca8035a2
Jan Zernisch <jan.zernisch@invision.de>
2022-06-03 23:48:42 +0200

    Add login form and make system tests pass

--- test/system/home_page_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class LoginPageTest < ApplicationSystemTestCase
-  test "visiting the home page" do
-    visit "/"
-
-    assert_selector "h1", text: "Home"
-  end
-end
--- test/system/login_test.rb
+++ test/system/login_test.rb
@@ -1,11 +1,19 @@
 require "application_system_test_case"
 
 class LoginTest < ApplicationSystemTestCase
+  test 'visiting root redirects to login if not logged in' do
+    visit '/'
+
+    assert_selector "h1", text: "Login"
+  end
+
   test "successful login redirects to home" do
     visit 'login'
 
-    fill_in 'email', with: 'foo@test.com'
-    fill_in 'password', with: 'c0rrectPW'
+    fill_in 'user_email', with: 'foo@bar.com'
+    fill_in 'user_password', with: 'password'
+
+    click_on 'Sign In'
 
     assert_selector "h1", text: "Home"
   end
@@ -13,8 +21,10 @@ class LoginTest < ApplicationSystemTestCase
   test "failed login redirects to login again" do
     visit 'login'
 
-    fill_in 'email', with: 'foo@test.com'
-    fill_in 'password', with: 'invalidPW'
+    fill_in 'user_email', with: 'foo@bar.com'
+    fill_in 'user_password', with: 'wrong_password'
+
+    click_on 'Sign In'
 
     assert_selector "h1", text: "Login"
   end
