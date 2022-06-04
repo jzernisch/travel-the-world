06e67cfba92976e5f9476bd6fb4062d1962f4453
Jan Zernisch <jan.zernisch@invision.de>
2022-06-05 01:27:35 +0200

    Improve controller test structure

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,109 +1,111 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test 'login page shows no logout button' do
-    get '/login'
-    assert_select "button", text: "Logout", count: 0
+  class NewTest < self
+    test 'sessions#new shows no logout button when not logged in' do
+      get '/login'
+      assert_select "button", text: "Logout", count: 0
+    end
+
+    test 'sessions#new redirects to root when already logged in' do
+      login_with_valid_credentials
+      get '/login'
+      assert_redirected_to root_path
+    end
+
+    test 'sessions#new shows a flash alert when already logged in' do
+      login_with_valid_credentials
+      get '/login'
+      assert_equal 'You are already logged in', flash[:alert]
+    end
   end
 
-  test "login returns 422 for a non-existing user" do
-    post '/login', params: { user: { email: 'no_name@bar.com', password: 'password' } }
-    assert_response 422
+  class CreateTest < self
+    test "sessions#create returns 422 for a non-existing user" do
+      login_with_non_existing_user
+      assert_response 422
+    end
+
+    test "sessions#create shows a flash alert for a non-existing user" do
+      login_with_non_existing_user
+      assert_equal 'Invalid credentials', flash[:alert]
+    end
+
+    test "sessions#create returns 422 for an existing user with invalid credentials" do
+      login_with_invalid_credentials
+      assert_response 422
+    end
+
+    test "sessions#create shows a flash alert for an existing user with invalid credentials" do
+      login_with_invalid_credentials
+      assert_equal 'Invalid credentials', flash[:alert]
+    end
+
+    test "sessions#create redirects to root after successful login" do
+      login_with_valid_credentials
+      assert_redirected_to root_path
+    end
+
+    test "sessions#create shows a flash notice after successful login" do
+      login_with_valid_credentials
+      assert_equal 'Successful login', flash[:notice]
+    end
+
+    test "sessions#create puts the user id in the session after successful login" do
+      user = login_with_valid_credentials
+      assert_equal user.id, session[:current_user_id]
+    end
+
+    test 'sessions#create shows a logout button after successful login' do
+      login_with_valid_credentials
+      follow_redirect!
+      assert_select "button", text: "Logout"
+    end
+
+    test 'sessions#create login shows a flash alert when already logged in' do
+      login_with_valid_credentials
+      post '/login'
+      assert_equal 'You are already logged in', flash[:alert]
+    end
   end
 
-  test "login shows a flash alert for a non-existing user" do
-    post '/login', params: { user: { email: 'no_name@bar.com', password: 'password' } }
-    assert_equal 'Invalid credentials', flash[:alert]
+  class DestroyTest < self
+    test 'sessions#destroy removes the user id from the session' do
+      login_with_valid_credentials
+      delete '/logout'
+      assert_nil session[:current_user_id]
+    end
+
+    test 'sessions#destroy redirects to login if user is not logged in' do
+      delete '/logout'
+      assert_redirected_to login_path
+    end
+
+    test 'sessions#destroy shows a flash notice' do
+      login_with_valid_credentials
+      delete '/logout'
+      assert_equal 'You have been signed out', flash[:notice]
+    end
+
+    test 'sessions#destroy hides the logout button' do
+      login_with_valid_credentials
+      delete '/logout'
+      assert_select "button", text: "Logout", count: 0
+    end
   end
 
-  test "login returns 422 for an existing user with invalid credentials" do
+  def login_with_invalid_credentials
     user = users(:one)
     post '/login', params: { user: { email: user.email, password: 'wrong_password' } }
-    assert_response 422
   end
 
-  test "login shows a flash alert for an existing user with invalid credentials" do
-    user = users(:one)
-    post '/login', params: { user: { email: user.email, password: 'wrong_password' } }
-    assert_equal 'Invalid credentials', flash[:alert]
+  def login_with_non_existing_user
+    post '/login', params: { user: { email: 'no_name@bar.com', password: 'password' } }
   end
 
-  test "login redirects to root after successful login" do
+  def login_with_valid_credentials
     user = users(:one)
     post '/login', params: { user: { email: user.email, password: 'password' } }
-    assert_redirected_to root_path
-  end
-
-  test "login shows a flash notice after successful login" do
-    user = users(:one)
-    post '/login', params: { user: { email: user.email, password: 'password' } }
-    assert_equal 'Successful login', flash[:notice]
-  end
-
-  test "login puts the user id in the session after successful login" do
-    user = users(:one)
-    post '/login', params: { user: { email: user.email, password: 'password' } }
-    assert_equal user.id, session[:current_user_id]
-  end
-
-  test 'login shows a logout button after successful login' do
-    user = users(:one)
-    post '/login', params: { user: { email: user.email, password: 'password' } }
-    follow_redirect!
-    assert_select "button", text: "Logout"
-  end
-
-  test 'login page redirects to root when already logged in' do
-    user = users(:one)
-    post '/login', params: { user: { email: user.email, password: 'password' } }
-    get '/login'
-    assert_redirected_to root_path
-  end
-
-  test 'login page shows a flash alert when already logged in' do
-    user = users(:one)
-    post '/login', params: { user: { email: user.email, password: 'password' } }
-    get '/login'
-    assert_equal 'You are already logged in', flash[:alert]
-  end
-
-  test 'login redirects to root when already logged in' do
-    user = users(:one)
-    post '/login', params: { user: { email: user.email, password: 'password' } }
-    post '/login'
-    assert_redirected_to root_path
-  end
-
-  test 'login shows a flash alert when already logged in' do
-    user = users(:one)
-    post '/login', params: { user: { email: user.email, password: 'password' } }
-    post '/login'
-    assert_equal 'You are already logged in', flash[:alert]
-  end
-
-  test 'logout removes the user id from the session' do
-    user = users(:one)
-    post '/login', params: { user: { email: user.email, password: 'password' } }
-    delete '/logout'
-    assert_nil session[:current_user_id]
-  end
-
-  test 'logout redirects to login if user is not logged in' do
-    delete '/logout'
-    assert_redirected_to login_path
-  end
-
-  test 'logout shows a flash notice' do
-    user = users(:one)
-    post '/login', params: { user: { email: user.email, password: 'password' } }
-    delete '/logout'
-    assert_equal 'You have been signed out', flash[:notice]
-  end
-
-  test 'logout hides the logout button' do
-    user = users(:one)
-    post '/login', params: { user: { email: user.email, password: 'password' } }
-    delete '/logout'
-    assert_select "button", text: "Logout", count: 0
+    user
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,17 +1,17 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "root redirects to login when not authenticated" do
+  test "static_pages#home redirects to login when not authenticated" do
     get '/'
     assert_redirected_to login_path
   end
 
-  test "root shows a flash notice when not authenticated" do
+  test "static_pages#home shows a flash notice when not authenticated" do
     get '/'
     assert_equal 'Please sign in first', flash[:alert]
   end
 
-  test "visit returns 200 when user is authenticated" do
+  test "static_pages#home visit returns 200 when user is authenticated" do
     user_params = { email: 'foo@bar.com', password: 'password' }
     user = User.create(user_params)
     post '/login', params: { user: { email: user.email, password: user.password } }
