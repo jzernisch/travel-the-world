5fe6829d45acc92e00cbb327091d585233a25a6a
Slawa Gladkov <svyatoslav.gladkov@gmail.com>
2022-06-06 00:07:22 +0200

    Update tests for protected controller

--- test/controllers/protected_controller_test.rb
+++ test/controllers/protected_controller_test.rb
@@ -1,8 +1,20 @@
 require "test_helper"
 
 class ProtectedControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+  include Devise::Test::IntegrationHelpers
+
+  test "it redirects to login page user which is not logged in" do
     get protected_home_url
+
+    assert_redirected_to new_user_session_path
+  end
+
+  test "it shows protected page when user is logged in" do
+    sign_in users(:sherlock)
+
+    get protected_home_url
+
     assert_response :success
+    assert_select "h1", "Protected"
   end
 end
