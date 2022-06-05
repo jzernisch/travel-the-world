3889663091f78823a59e6329dacfdfd8a3968c11
Slawa Gladkov <svyatoslav.gladkov@gmail.com>
2022-06-06 00:16:52 +0200

    Add color highlight to the command which is being run from the docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
+HIGHLIGHT_COLOR='\033[1;35m'
+NO_COLOR='\033[0m'
+
 rm -f tmp/pids/server.pid
 
-echo Executing command $@
+echo -e Executing command${HIGHLIGHT_COLOR} $@${NO_COLOR}
 exec "$@"
